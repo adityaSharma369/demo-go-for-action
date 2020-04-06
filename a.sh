eddb7c5ad1393e5f5b689e14735a72e299855592
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-04-07 00:54:33 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,5 @@
 number=$(jq .version package.json)
 version=$(echo $number | tr -d '"')
 sudo docker build --no-cache --build-arg version_default=$version -t adityasharma369/hello-adi:$version -f Dockerfile . 
+sudo docker login --username=adityasharma369 --password=Adi@.3690
 sudo docker push adityasharma369/hello-adi:$version
