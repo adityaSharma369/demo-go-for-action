6b022555b5936d73ae738321c59b7ef2fd57a1e6
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-04-10 15:01:56 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -9,7 +9,7 @@ sudo docker push gcr.io/gaadiman/hello-adi:$version
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi
+$(aws ecr get-login --region us-east-1 --no-include-email)
 sudo docker tag hello-adi:$version 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi:$version
 sudo docker push 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi:$version
 
