530f6fb749adfccd38c46d789dce8b933080f409
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-04-10 12:13:22 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -6,3 +6,11 @@ sudo docker login --username=adityasharma369 --password=Adi@.3690
 sudo docker push adityasharma369/hello-adi:$version
 sudo docker tag hello-adi:$version gcr.io/gaadiman/hello-adi:$version
 sudo docker push gcr.io/gaadiman/hello-adi:$version
+    - name: Push to ECR
+      id: ecr
+      uses: jwalton/gh-ecr-push@v1.0.0
+      with:
+        access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+        secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+        region: us-east-2
+        image: hello-adi:$version
