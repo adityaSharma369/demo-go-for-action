8c34385a6ac289556482fec3740a8b9f1748cd2c
adityaSharma369 <53860201+adityaSharma369@users.noreply.github.com>
2020-04-13 16:22:16 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -6,14 +6,14 @@ sudo docker login --username=adityasharma369 --password=Adi@.3690
 sudo docker push adityasharma369/hello-adi:$version
 sudo docker tag hello-adi:$version gcr.io/gaadiman/hello-adi:$version
 sudo docker push gcr.io/gaadiman/hello-adi:$version
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-sudo chown $USER:docker ~/.docker
-sudo chown $USER:docker ~/.docker/config.json
-sudo chmod g+rw ~/.docker/config.json
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi
-sudo docker tag hello-adi:$version 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi:$version
-sudo docker push 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi:$version
+# sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+# sudo chown $USER:docker ~/.docker
+# sudo chown $USER:docker ~/.docker/config.json
+# sudo chmod g+rw ~/.docker/config.json
+# aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi
+# sudo docker tag hello-adi:$version 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi:$version
+# sudo docker push 985877142670.dkr.ecr.us-east-2.amazonaws.com/hello-adi:$version
 
 
